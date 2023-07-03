48113899eb027aefd84a9c26765d38a70194fafe
Uriel-PP <kaattoo9@gmail.com>
2023-07-02 21:23:39 -0600

    Initial Commit

--- src/Add_book.java
+++ src/Add_book.java
@@ -9,10 +9,13 @@ public class Add_book {
 
     public Add_book(String Name, int Editorial, String Autor, int Ano){
         this.Name = Name;
+        this.Editorial = Editorial;
+        this.Autor= Autor;
+        this.Ano = Ano;
         System.out.println("Name: " +Name + " Editorial: " + Editorial +" Autor: " + Autor + " Year: " + Ano);
     }
 
     public void add(){
-
+        System.out.println("Name: " +Name + " Editorial: " + Editorial +" Autor: " + Autor + " Year: " + Ano);
     }
 }
